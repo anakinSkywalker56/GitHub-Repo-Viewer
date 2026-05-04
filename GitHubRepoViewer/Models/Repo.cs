3471f6f72f0666600bb3e6405b0ed8b18fe133a4
Anakin <johninjug56@gmail.com>
2026-05-04 21:55:57 +0800

    Added models for Users, and added DBContext from scaffold

--- GitHubRepoViewer/Models/Repo.cs
+++ GitHubRepoViewer/Models/Repo.cs
@@ -5,12 +5,12 @@ namespace GitHubRepoViewer.Models
     public class Repo
     {
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [JsonPropertyName("html_url")]
-        public string Html_url { get; set; }
+        public string? Html_url { get; set; }
         [JsonPropertyName("stargazers_count")]
         public int Stargazers_count { get; set; }
         [JsonPropertyName("language")]
-        public string Language { get; set; }
+        public string? Language { get; set; }
     }
 }
